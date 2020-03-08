145c87dff1ccacefd7b89499ce23886952bae55b
Parijat Purohit <parijat28purohit@gmail.com>
2020-03-08 13:25:44 +0530

    replace generate scripts. reorganise protobuf

--- pb/generate.sh
+++ pb/generate.sh
@@ -1,4 +1,3 @@
 cd $GOPATH/src
 mkdir -p github.com/hello/generated
-protoc --go_out=github.com/hello/generated/ github.com/hello/pb/storage/user/schema.proto
-protoc --proto_path=github.com/hello/pb/storage/user/ --go_out=github.com/hello/generated/ github.com/hello/pb/storage/user/FindByRollAndName.proto
+protoc --proto_path=github.com/hello/pb/storage --go_out=plugins=grpc:github.com/hello/generated/ github.com/hello/pb/storage/*.proto

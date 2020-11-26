3aaa38e6f2433696caf6190980129749f8954b90
Parijat Purohit <parijat28purohit@gmail.com>
2020-11-26 16:08:36 +0800

    update proto protoc gen go to use go get instead of homebrew

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-which -s brew
-if [[ $? != 0 ]] ; then
-    # Install Homebrew
-    echo "brew not installed. installing now"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-else
-    echo "brew installed.. checking for updates"
-    brew update
-fi
-
 which -s go
 if [[ $? != 0 ]] ; then
   echo "go not installed. installing now"
@@ -27,4 +17,4 @@ go get -u gopkg.in/yaml.v2 github.com/go-sql-driver/mysql github.com/jinzhu/gorm
 
 export PATH=$PATH:/$GOPATH/bin
 
-./generate.sh go
+./generator.sh go
